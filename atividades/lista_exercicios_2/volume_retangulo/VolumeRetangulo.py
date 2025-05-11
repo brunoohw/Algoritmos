11608e7219cd329145b2e52eb30c76eb58ed80f7
brunoohw <brunoohw2410@gmail.com>
2025-05-11 14:43:55 -0300

    solução exercício volume retângulo

---
+++ atividades/lista_exercicios_2/volume_retangulo/VolumeRetangulo.py
@@ -0,0 +1,7 @@
+Comprimento = float(input("Digite o comprimento do retângulo: "))
+Largura = float(input("Digite a largura do retângulo: "))
+Altura = float(input("Digite a altura do retângulo: "))
+
+Volume = Comprimento * Largura * Altura
+
+print("O volume do retângulo é de", Volume)
