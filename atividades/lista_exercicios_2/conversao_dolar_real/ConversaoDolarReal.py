cfd5e2ca448cc86a11037975d8ee3d63506c4dc7
brunoohw <brunoohw2410@gmail.com>
2025-05-12 10:39:42 -0300

    soluciona exercicio conversao dolar real

---
+++ atividades/lista_exercicios_2/conversao_dolar_real/ConversaoDolarReal.py
@@ -0,0 +1,6 @@
+Dolar = float(input("Digite um valor em dólar: "))
+Cotacao = float(input("Digite o valor da cotação atual do dólar: "))
+
+Real = Dolar * Cotacao
+
+print("O valor em dólar foi de US$", Dolar, "E sua conversão com a cotação de R$", Cotacao,"Foi de R$",Real)
