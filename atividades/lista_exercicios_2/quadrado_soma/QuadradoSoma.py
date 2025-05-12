372840d0eb3cc3a409284b4759532b7a3e31efde
brunoohw <brunoohw2410@gmail.com>
2025-05-12 10:45:11 -0300

    soluciona exercício quadrado soma

---
+++ atividades/lista_exercicios_2/quadrado_soma/QuadradoSoma.py
@@ -0,0 +1,7 @@
+A = int(input("Digite o valor de A: "))
+B = int(input("Digite o valor de B: "))
+C = int(input("Digite o valor de C: "))
+
+Soma = (A + B + C) ** 2
+
+print("O quadrado da soma dos valores A, B e C é de", Soma)
