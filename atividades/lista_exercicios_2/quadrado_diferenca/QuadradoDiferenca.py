22b8d9f33cf6040b38b773344a161da24206cd65
brunoohw <brunoohw2410@gmail.com>
2025-05-12 10:30:35 -0300

    soluciona exercicio quadrado diferenca

---
+++ atividades/lista_exercicios_2/quadrado_diferenca/QuadradoDiferenca.py
@@ -0,0 +1,6 @@
+A = int(input("Digite o valor de A: "))
+B = int(input("Digite o valor de B: "))
+
+Diferenca = (A - B) ** 2
+
+print("O resultado do quadrado da diferença entre A e B é de", Diferenca)
