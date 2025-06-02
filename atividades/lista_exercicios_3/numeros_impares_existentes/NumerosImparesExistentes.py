fc4d82c4b6906dd57a6878d4b0c15cc0310ffe98
brunoohw <brunoohw2410@gmail.com>
2025-06-02 17:58:07 -0300

    Possível solução ao exercício numeros impares existentes

--- atividades/lista_exercicios_3/numeros_impares_existentes/NumerosImparesExistentes.py
+++ atividades/lista_exercicios_3/numeros_impares_existentes/NumerosImparesExistentes.py
@@ -4,13 +4,13 @@ N2 = int(input("Digite um segundo valor inteiro positivo: "))
 print("")
 
 print("Múltiplos do primeiro Número")
-for i in range(1, 10):
-    N1 = N1 % 2 == 0
+for i in range(1, N1 % 2 == 1):
+
     print("Os números múltiplos do primeiro número são: ", N1)
 
 print("")
 
 print("Múltiplos segundo Número")
 for i in range(1, 5):
-    N2 = N2 % 2 == 0
+    N2 = N2 % 2 == 1
     print("Os números múltiplos do segundo número são: ", N2)
