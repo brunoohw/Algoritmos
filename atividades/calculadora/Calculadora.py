8e388e133acba9d57d358b702c0fe585e0a75a70
brunoohw <brunoohw2410@gmail.com>
2025-06-02 22:54:42 -0300

    Solução para exercício de criar uma calculadora tratando divisão por 0

--- atividades/calculadora/Calculadora.py
+++ atividades/calculadora/Calculadora.py
@@ -1,75 +1,46 @@
-Valor1 = float(input("Digite o primeiro valor: "))
+Valor1 = float(input("Digite um valor: "))
+Acumulador = Valor1
 OP = str(input("Digite qual será a operação a ser feita: "))
-Valor2 = float(input("Digite o segundo valor: "))
-#Valor3 = str(input("Você deseja continuar a fazer cálculos? (S/N): "))
 
-Soma = Valor1 + Valor2
-Sub = Valor1 - Valor2
-Mult = Valor1 * Valor2
-Div = Valor1 / Valor2
+if OP != "=":
+    while OP != "=":
+        if OP == "+":
+            Valor1 = float(input("Digite um valor: "))
+            Soma = Acumulador + Valor1
+            Acumulador = Soma
+            OP = str(input("Digite qual será a operação a ser feita: "))
+            if OP == "=":
+                print(Acumulador)
+        if OP == "-":
+            Valor1 = float(input("Digite um valor: "))
+            Sub = Acumulador - Valor1
+            Acumulador = Sub
+            OP = str(input("Digite qual será a operação a ser feita: "))
+            if OP == "=":
+                print(Acumulador)
+        if OP == "*":
+            Valor1 = float(input("Digite um valor: "))
+            Mult = Acumulador * Valor1
+            Acumulador = Mult
+            OP = str(input("Digite qual será a operação a ser feita: "))
+            if OP == "=":
+                print(Acumulador)
+        if OP == "/":
+            Valor1 = float(input("Digite um valor: "))
+            if Valor1 == 0:
+                print("Não é possível realizar uma divisão por zero.")
+                Valor1 = float(input("Digite outro valor: "))
+            Div = Acumulador / Valor1
+            Acumulador = Div
+            OP = str(input("Digite qual será a operação a ser feita: "))
+            if OP == "=":
+                print(Acumulador)
+else:
+    print(Valor1)
 
-while OP != "=":
-    if OP == "+":
-        print("Este é o resultado da Soma: ", Soma)
-    elif OP == "-":
-        if Valor1 > Valor2:
-            Sub = Valor1 - Valor2
-            print("Este é o resultado da Subtração: ", Sub)
-        else:
-            Sub = Valor2 - Valor1
-            print("Este é o resultado da Subtração: ", Sub)
-    elif OP == "*":
-       print("Este é o resultado da Multiplicação: ", Mult)
-    elif OP == "/":
-        print("Este é o resultado da Divisão: ", Div)
-    OP = str(input("Digite qual será a operação a ser feita: "))
-    Valor1 = float(input("Digite o primeiro valor: "))
 
 
 
 
 
-#if Valor3 == "N":
-#    if OP == "+":
-#        print("Este é o resultado da Soma: ", Soma)
-#    elif OP == "-":
-#        if Valor1 > Valor2:
-#            Sub = Valor1 - Valor2
-#            print("Este é o resultado da Subtração: ", Sub)
-#        else:
-#            Sub = Valor2 - Valor1
-#            print("Este é o resultado da Subtração: ", Sub)
-#    elif OP == "*":
-#        print("Este é o resultado da Multiplicação: ", Mult)
-#    elif OP == "/":
-#        print("Este é o resultado da Divisão: ", Div)
-
-#while Valor3 == "S":
-#    OP = str(input("Digite qual será a operação a ser feita: "))
-#    Valor1 = float(input("Digite o primeiro valor: "))
-#    OP = str(input("Digite qual será a operação a ser feita: "))
-#    Valor2 = float(input("Digite o segundo valor: "))
-#    Valor3 = str(input("Você deseja continuar a fazer cálculos? (S/N): "))
-
-#    if OP == "+":
-#        Soma = Soma + (Valor1 + Valor2)
-#    elif OP == "-":
-#        if Valor1 > Valor2:
-#            Sub = Sub - Sub
-#        else:
-#            Sub = Sub - (Valor2 - Valor1)
-#    elif OP == "*":
-#        Mult = Mult * (Valor1 * Valor2)
-#    elif OP == "/":
-#        Div = Div / (Valor1 / Valor2)
-
-#    if Valor3 == "N":
-#        if OP == "+":
-#            print("Este é o resultado da Soma: ", Soma)
-#        elif OP == "-":
-#                print("Este é o resultado da Subtração: ", Sub)
-#        elif OP == "*":
-#            print("Este é o resultado da Multiplicação: ", Mult)
-#        elif OP == "/":
-#            print("Este é o resultado da Divisão: ", Div)
 
