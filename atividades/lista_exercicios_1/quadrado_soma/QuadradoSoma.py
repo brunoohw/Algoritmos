1bc99890096e1bd855a27e979b760333452f338c
brunoohw <brunoohw2410@gmail.com>
2025-06-10 00:16:46 -0300

    Soluciona exercício Apresentar o valor do Quadrado da Soma de Três valores lidos

---
+++ atividades/lista_exercicios_1/quadrado_soma/QuadradoSoma.py
@@ -0,0 +1,7 @@
+A = int(input("Digite o valor de A: "))
+B = int(input("Digite o valor de B: "))
+C = int(input("Digite o valor de C: "))
+
+QuadradoSoma = (A + B + C) ** 2
+
+print(f"O quadrado da soma dos valores {A}, {B} e {C} é {QuadradoSoma}")
