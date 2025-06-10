40863150e90c6523c46023930e5e941a804caf4d
brunoohw <brunoohw2410@gmail.com>
2025-06-09 23:59:42 -0300

    Soluciona exercício Apresentar o Quadrado da Diferença do Valor A entre o Valor B

---
+++ atividades/lista_exercicios_1/quadrado_diferenca/QuadradoDiferença.py
@@ -0,0 +1,6 @@
+A = int(input("Digite o valor de A: "))
+B = int(input("Digite o valor de B: "))
+
+QuadradoDiferenca = (A - B) ** 2
+
+print(f"O quadrado da diferença de {A} e {B} é {QuadradoDiferenca}!")
