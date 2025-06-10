2a53628dc7bb3d18d66bc81c03d97c08c04a7dbb
brunoohw <brunoohw2410@gmail.com>
2025-06-10 00:11:01 -0300

    Soluciona exercício Conversão de Dólares em Reais lendo o valor da Cotação

---
+++ atividades/lista_exercicios_1/conversao_dolar_real/ConversaoDolarReal.py
@@ -0,0 +1,6 @@
+Dolar = float(input("Digite a quantidade em Dólares: "))
+Cotacao = float(input("Digite o valor da cotação atual do dólar: "))
+
+Real = Dolar * Cotacao
+
+print(f"US${Dolar} Dólares com a cotação de R${Cotacao} reais o dólar, são R${Real} Reais!")
