f8c5bc3b116e771c7a49825be57236966384fed5
brunoohw <brunoohw2410@gmail.com>
2025-06-09 23:48:16 -0300

    Soluciona exercício Apresentar Volume de Caixa Retangular

---
+++ atividades/lista_exercicios_1/volume_caixa_retangular/VolumeCaixaRetangular.py
@@ -0,0 +1,7 @@
+Comprimento = float(input("Digite o comprimento do rentângulo: "))
+Largura = float(input("Digite a largura do retângulo: "))
+Altura = float(input("Digite a altura do retângulo: "))
+
+Volume = Comprimento * Largura * Altura
+
+print(f"O volume deste retângulo é de {Volume}!")
