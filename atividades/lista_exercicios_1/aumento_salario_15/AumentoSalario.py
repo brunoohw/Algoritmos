c64ebd045148f21793dad466940ebb93e257cedc
brunoohw <brunoohw2410@gmail.com>
2025-06-09 23:08:54 -0300

    Soluciona exercício de Aumento de Salário em 15%

---
+++ atividades/lista_exercicios_1/aumento_salario_15/AumentoSalario.py
@@ -0,0 +1,6 @@
+Salario = float(input("Digite o seu salário: "))
+
+Acrescimo = Salario * 0.15
+NovoSalario = Salario + Acrescimo
+
+print("Seu salário de R$",Salario,"com acréscimo de 15% que é R$",Acrescimo,"ficou em R$",NovoSalario,"!")
